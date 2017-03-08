097087197f1b9ffa855e08396f0736a224876e84
jacob <jacob.butler1994@gmail.com>
2017-03-08 07:49:13 -0800

    added a wordbank and a variable that is defined by a random word within the wordBank.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("JS is linked");
+window.onload = function() {
+
+	var keyboard = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 
+		'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
+		'z', 'x', 'c', 'v', 'b', 'n', 'm']
+
+	// list of possible words
+	var wordBank = ["computer", "program", "mouse", "keyboard", "javascript", "css", "html", "website"];
+	
+	// the word to guess
+	var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+	
+	var guessedLetters; // letters that have been guessed.
+	var blankSpaces // blank spaces to be filled by correct answers.
+
+}
