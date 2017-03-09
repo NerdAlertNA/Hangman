d81b1c30a6e56d765d5d6e44baf2ebc5e4b425fa
jacob <jacob.butler1994@gmail.com>
2017-03-09 12:43:16 -0800

    added button and functionality

--- js/main.js
+++ js/main.js
@@ -1,27 +1,54 @@
 window.onload = function() {
 
-	var keyboard = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 
-		'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
-		'z', 'x', 'c', 'v', 'b', 'n', 'm']
-
 	// list of possible words
 	var wordBank = ["computer", "program", "mouse", "keyboard", "javascript", "css", "html", "website"];
 	
-	var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; // Randomly selects a word from wordBank
-	
-	var wordToGuess = randomWord.split(''); // splits the chosen word into a new array
+	var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; // randomly selects a word from wordBank
 
-	for (var i = 0; i < wordToGuess.length; i++) { // replaces the word to be displayed with underscores
-		wordToGuess[i] = '_';
+	var wordToGuessSplit = randomWord.split(''); // splits the chosen word into a new array	
+
+	var wordToGuess = new Array(wordToGuessSplit.length);
+	
+
+	console.log(randomWord);
+	console.log(wordToGuessSplit);
+	console.log(wordToGuess);
+
+	for (var i = 0; i < wordToGuess.length; i++) {
+		wordToGuess[i] = '_ ';
 	}
+
+	console.log(wordToGuess);
+// GIVEN I am on the game page
+// WHEN I type a letter 
+// AND the letter is not in the word
+// THEN display a message "INCORRECT" 
+
 	
-	var guessedLetters; // letters that have been guessed.
-	
-
-
-	
-	document.getElementById("word").innerHTML = wordToGuess.join(' ');// display the word in the game.
 
 
 
-}
+
+
+
+
+
+	/*var printWordToGuess = function() {
+		for (var i = 0; i < wordToGuess.length; i++) {
+			var word = document.getElementById('word');
+			var printedWord = document.createTextNode(wordToGuess[i]);
+			word.appendChild(printedWord);
+		}*/
+	//}
+
+	// 	$("p").on("click", function(){
+//     alert("The paragraph was clicked.");
+// });$("input:text").val("Glenn Quagmire");
+
+	$('#submit').on('click', function() {
+		var x = $('#guessInput').val();
+		alert(x);
+	});
+};
+
+
