780764618033bd3997032881dbc7deead1c61352
jacob <jacob.butler1994@gmail.com>
2017-03-09 13:59:46 -0800

    added functions for alerts if you guess correctly and an alert that tells you you have lost after too many wrong guesses.

--- js/main.js
+++ js/main.js
@@ -5,58 +5,31 @@ window.onload = function() {
 	
 	var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; // randomly selects a word from wordBank
 
-	var wordToGuessSplit = randomWord.split(''); // splits the chosen word into a new array	
+	var wrongGuess = 0;
 
-	var wordToGuess = new Array(wordToGuessSplit.length);
-	
-
+	// var wordToGuessSplit = randomWord.split(''); // splits the chosen word into a new array	
 	console.log(randomWord);
-	console.log(wordToGuessSplit);
-	console.log(wordToGuess);
 
 
-// var n = str.includes("world");
-	$('#submit').on('click', function() { // event listener
-		var guessedLetter = $('#guessInput').val();
-		if (!wordToGuess.includes(guessedLetter)) {
+
+	$('#submit').on('click', function() { // takes player input and compares to string
+		var guessedLetter = $('#guessInput').val(); // to determin if there is a match
+		if (!randomWord.includes(guessedLetter)) {
 			alert('INCORRECT');
+			wrongGuess++;
+			console.log(wrongGuess);
+			if (wrongGuess === 9) {
+				alert('You are dead!');
+			}
 		}
-
-			
+		else {
+			alert('CORRECT');
+		}
 	});
+
 };
 
 
-	// for (var i = 0; i < wordToGuess.length; i++) {
-	// 	wordToGuess[i] = '_ ';
-	// }
-
-	//console.log(wordToGuess);
-// GIVEN I am on the game page
-// WHEN I type a letter 
-// AND the letter is not in the word
-// THEN display a message "INCORRECT" 
-
-	
-
-
-
-
-
-
-
-
-	/*var printWordToGuess = function() {
-		for (var i = 0; i < wordToGuess.length; i++) {
-			var word = document.getElementById('word');
-			var printedWord = document.createTextNode(wordToGuess[i]);
-			word.appendChild(printedWord);
-		}*/
-	//}
-
-	// 	$("p").on("click", function(){
-//     alert("The paragraph was clicked.");
-// });$("input:text").val("Glenn Quagmire");
 
 
 
