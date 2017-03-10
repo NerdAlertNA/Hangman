5eeeed3923633832c52e13a2a77075e614ca7e30
jacob <jacob.butler1994@gmail.com>
2017-03-09 17:53:16 -0800

    touched up code for readability

--- js/main.js
+++ js/main.js
@@ -5,73 +5,44 @@ window.onload = function() {
 	
 	var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; // randomly selects a word from wordBank
 
-	// var randomWordArr = randomWord.split('');
-
 	var wrongLettersArray = [];
 
 	var rightLettersArray = [];
 
 	var wrongGuess = 0;
 
-	// console.log(randomWordArr);
-	// for (var i = 0; i < randomWordArr.length; i++) {  //convert randomWord to undersores.
-	// 	randomWordArr[i] = "_";
-	// }
-
-	// document.getElementById('word').innerHTML = randomWordArr.join(' ');
-
-
 	console.log(randomWord);
-	// console.log(randomWordArr);
 	console.log(wrongGuess);
-	console.log(wrongLettersArray);
-	console.log(rightLettersArray);
-
-
 
 	$('#submit').on('click', function() { // takes player input and compares to string
 		var guessedLetter = $('#guessInput').val(); // to determin if there is a match
 
-		if (!wrongLettersArray.includes(guessedLetter) && !rightLettersArray.includes(guessedLetter)){
+		if (!wrongLettersArray.includes(guessedLetter) && !rightLettersArray.includes(guessedLetter)){ // checks if letter has been guessed
 			if (!randomWord.includes(guessedLetter)) {
-				alert('INCORRECT');
-				wrongGuess++;
-				wrongLettersArray.push(guessedLetter);
+				alert('INCORRECT');						 // if guess is guess is wrong
+				wrongGuess++;							 // alert "INCORRECT"
+				wrongLettersArray.push(guessedLetter);	 // and add letter to the incorrect guess array
 				console.log(wrongGuess);
 				console.log(wrongLettersArray);
-				if (wrongGuess === 9) {
+				if (wrongGuess === 9) {					// if you guess wrong 9 times you lose.
 					alert('You are dead!');
 				}
 			}
 			else {
 				alert('CORRECT');
-				// rightLettersArray.push(guessedLetter);
-				for (var i = 0; i < randomWord.length; i++) {
-					if (randomWord[i] === guessedLetter) {
-						rightLettersArray.push(guessedLetter);
-						console.log(rightLettersArray);
+				for (var i = 0; i < randomWord.length; i++) { 	// cycles through each letter of the current word
+					if (randomWord[i] === guessedLetter) {    	// and if a match is found
+						rightLettersArray.push(guessedLetter);	// adds the letter to the correct guess array
+						console.log(rightLettersArray);		  	// even through multiple itterations.
 					}
 				}
-				// for (var i = 0; i < randomWordArr.length; i++) {
-				// 	if(randomWord[i] === guessedLetter) {
-				// 		randomWordArr[i] = guessedLetter + ' ';
-				// 	}
-				// }
-				// for (var i = 0; i < randomWord.length; i++) {
-				// 	for (var j = 0; j < rightLettersArray.length; j++) {
-				// 		if (randomWord.includes(guessedLetter) && !rightLettersArray.includes(guessedLetter)) {
-				// 			rightLettersArray.push(guessedLetter);
-				// 			console.log(rightLettersArray);
-				// 		}
-				// 	}
-				// }
-				if (rightLettersArray.length === randomWord.length) {
-					alert('You survived!');
+				if (rightLettersArray.length === randomWord.length) {   // if .lenth of array === .length of word
+					alert('You survived!');								// alert "You survived!"
 				}
 			}
 		}
 		else {
-			alert('You have already guessed that letter');
+			alert('You have already guessed that letter'); // if letter has been guess display this
 		}
 	});
 
